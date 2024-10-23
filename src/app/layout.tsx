8556fe697ce6dc21388ebee609d4e1ff945829bd
unknown <jonatanlopezbello@hotmail.com>
2024-10-23 11:28:12 -0500

    error metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
+"use client";
 import { baselightTheme } from "@/utils/theme/DefaultColors";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { Metadata } from "next";
 
-// Definir metadata
-export const metadata: Metadata = {
-  title: "Perfil de Usuario - Gestión de Créditos",
-  description: "Visualiza y gestiona el perfil de usuario, incluyendo el saldo pendiente, cuotas pagadas y el historial de créditos.",
-  keywords: ["usuario", "perfil", "crédito", "saldo pendiente", "historial de créditos", "gestión financiera"],
-  authors: [{ name: "Tu Nombre o Compañía" }],
-  viewport: "width=device-width, initial-scale=1.0",
-};
 
 export default function RootLayout({
   children,
