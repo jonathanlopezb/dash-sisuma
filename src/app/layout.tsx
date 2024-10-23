1c1cf8d526b4327a8eda7462e28380934dc11c2a
unknown <jonatanlopezbello@hotmail.com>
2024-10-23 10:31:43 -0500

    error metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
 "use client";
-import type { Metadata } from "next";
 import { baselightTheme } from "@/utils/theme/DefaultColors";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
-export const metadata: Metadata = {
-  title: "SISUMA - Creditos al instante.",
-  description: "Somos una empresa de créditos especializada en ofrecer soluciones financieras rápidas y seguras. Nuestro compromiso es facilitarte el acceso a financiamiento para que alcances tus metas de manera eficiente y sin complicaciones.",
-};
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -15,7 +10,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+
     <html lang="en">
+      <Head>
+        {/* Metadata */}
+        <title>Perfil de Usuario - Gestión de Créditos</title>
+        <meta name="description" content="Visualiza y gestiona el perfil de usuario, incluyendo el saldo pendiente, cuotas pagadas y el historial de créditos." />
+        <meta name="keywords" content="usuario, perfil, crédito, saldo pendiente, historial de créditos, gestión financiera" />
+        <meta name="author" content="SISUMA" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <ThemeProvider theme={baselightTheme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
